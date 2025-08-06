8332fa15fea31a756e4a5ff6fac8f655c8f654a0
Работа <git@github.com:ILYA-NSK73/Work-1.git>
2025-08-06 20:26:46 +0400

    Использовал корректировку кода с помощью Ctrl alt l. Спасибо, что показал, что-то новое!

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,44 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         System.out.println("Задача №1");
         var dog = 8.0;
-       System.out.println(dog);
-       var cat = 3.6;
-       System.out.println(cat);
-       var paper = 763789;
-       System.out.println(paper);
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
 
         System.out.println("Задача №2");
         dog = dog * 4;
         System.out.println(dog);
         cat = cat * 4;
         System.out.println(cat);
-        paper = paper *4;
+        paper = paper * 4;
         System.out.println(paper);
 
         System.out.println("Задача №3 (Возможно я сделал не верно, потому что по своему понял задание!! Но итоговый результат я считаю верен!");
         dog = dog - 3.5;
-       System.out.println(dog);
-       cat = cat - 1.6;
-       System.out.println(cat);
-       paper = paper - 7639;
-       System.out.println(paper);
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
 
         System.out.println("Задача №4");
         var friend = 19;
         System.out.println(friend);
-        friend = friend +2;
+        friend = friend + 2;
         System.out.println(friend);
-        friend = friend /7;
+        friend = friend / 7;
         System.out.println(friend);
 
         System.out.println("Задача №5");
         var frog = 3.5;
         System.out.println(frog);
-        frog = frog *10;
+        frog = frog * 10;
         System.out.println(frog);
-        frog = frog /3.5;
+        frog = frog / 3.5;
         System.out.println(frog);
         frog = frog + 4;
         System.out.println(frog);
@@ -59,24 +59,15 @@ public class Main {
 
         System.out.println("Задача №8");
         var totalHours = 640;
-        System.out.println("Всего " + totalHours +  " часов");
+        System.out.println("Всего " + totalHours + " часов");
         var worker1 = 8;
         System.out.println("Один сотрудник посвещает работе " + worker1 + " часов");
         var totalWorker = totalHours / worker1;
-        System.out.println("Всего работник в комании " + totalWorker +   " человек");
+        System.out.println("Всего работник в комании " + totalWorker + " человек");
         totalWorker = totalWorker + 94;
         System.out.println("Если в комании работает " + totalWorker + " человека");
         var totalHours1 = totalHours / totalWorker;
         System.out.println("то всего " + totalHours1 + " по часа работы может быть поделено между сотрудниками");
-
-
-
-
-
-
-
-
-
 
     }
 }
